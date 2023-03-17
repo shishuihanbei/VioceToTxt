2c8e8af8b75d8f50932ee097f7664ec2278bb969
shishuihanbei <31913640+shishuihanbei@users.noreply.github.com>
2023-03-17 16:34:48 +0800

    Add files via upload

---
+++ VideoAudiotoTxt.py
@@ -0,0 +1,105 @@
+from moviepy.editor import VideoFileClip,AudioFileClip
+import os
+import requests
+import json
+import time
+
+class LenovoFileToTxt():
+	def __init__(self,file):
+		self.videoName = os.path.split(file)[1]
+		self.videoSize = os.path.getsize(file)
+		self.period = int(AudioFileClip(file).duration*1000)
+		self.file = file
+		self.url = 'https://smart.lenovo.com.cn/audioservice'
+		self.headers = {
+			"Tokencui":self.getCuiToken(),
+			"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41"
+		}
+
+	def getCuiToken(self):
+		headers = {
+			"Content-Type":"application/json",
+			"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41"
+		}
+		data = {
+			"lenovo_id_info": {
+				"realm": "api.iot.lenovomm.com",
+				"ticket": ""
+			},
+			"client_key": "60a3327c94df8b95f0ccb7a4",
+			"device_info": {
+				"device_id": "1a5ee779-bd34-4374-b7bd-bb7868a6961b",
+				"mac": "",
+				"vendor": "UNKNOW_TYPE",
+				"hw_model": "Chrome_Text_Translation",
+				"client_sw_ver": '2.0.0'
+			},
+			"product_id": "Chrome_Text_Translation",
+		}
+		cui = "https://cuiauth.lenovo.com.cn:443/auth/v1/simpletoken"
+		resp = requests.post(url = cui,data = json.dumps(data),headers = headers,verify = False)
+		result = resp.json()
+		tokencui = result['data']['access_token']
+		return tokencui
+
+	def fileupload(self):
+		"""音视频文件上传"""
+		upload_url = self.url + "/voice/wav2txt"
+		params = {
+			"language":"cn",
+			"videoName":self.videoName,
+			"videoSize":self.videoSize,
+			"period":self.period
+		}
+		files = {'file':(self.videoName,open(self.file,'rb'))}
+		resp = requests.post(url = upload_url,params = params,files= files,headers = self.headers,verify =False)
+		taskId = resp.json()['res']['taskId']
+		return taskId
+
+	def getTaskStatus(self,taskId):
+		"""获取任务状态"""
+		task_status_url = self.url + "/voice/getTaskStatus"
+		params = {"taskId":taskId}
+		resp = requests.get(url = task_status_url, headers = self.headers, params = params, verify =False)
+		result = resp.json()
+		return result
+
+	def deleteTask(self,taskId):
+		"""删除任务"""
+		del_url = self.url + "/voice/deleteTask"
+		params = {
+			"taskId":taskId
+		}
+		resp = requests.get(url = del_url, headers = self.headers, params = params,verify =False)
+		result = resp.json()
+		status = result['status']
+		# if status == "Y":
+		# 	print(self.videoName + "文件删除成功！")
+
+	def getTaskTxt(self):
+		"""获取完成的文件并删除任务"""
+		taskId = self.fileupload()
+		flag = True
+		while flag:
+			result = self.getTaskStatus(taskId)
+			translateTime = result['res']['translateTime']
+			if translateTime == "已完成":
+				flag = False
+				self.deleteTask(taskId)
+				txt = "\n".join([i['onebest'] for i in json.loads(result["res"]["asrTxt"])])
+				return self.videoName,txt
+			time.sleep(1)
+
+def main():
+	file = "C:/Users/Administrator/Desktop/鹏飞聊教培--音频/"
+	for i in os.listdir(file):
+		print("正在处理",i)
+		f = os.path.join(file,i)
+		file_name,txt = LenovoFileToTxt(f).getTaskTxt()
+		with open(os.path.join(file,file_name.rsplit(".",1)[0].strip()+'.txt'),'w',encoding="utf-8") as f:
+			f.write(txt)
+		# path = os.path.abspath(f)
+		print(file)
+
+if __name__ == '__main__':
+	main()
---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+import json
+import shutil
+from moviepy.editor import VideoFileClip,AudioFileClip
+from VideoAudiotoTxt import LenovoFileToTxt
+import time
+
+def getTxt(txt_path,video_file):
+	print("正在处理：",os.path.split(video_file)[-1])
+	file_name,txt = LenovoFileToTxt(video_file).getTaskTxt()
+	with open(os.path.join(txt_path,file_name.rsplit(".",1)[0].strip()+'.txt'),'w',encoding="utf-8") as f:
+		f.write(txt)
+	print("处理完成：",os.path.split(video_file)[-1])
+
+def mp4tomp3(path,file):
+	if not file.endswith('.mp3'):
+		clip = VideoFileClip(file)
+		audio = clip.audio
+		fpath,fname = os.path.split(file)
+		fname,ftype = os.path.splitext(fname)
+		file_mp3 = os.path.join(path,fname) + '.mp3'
+		l = audio.write_audiofile(file_mp3)
+		return file_mp3
+	else:
+		print('不是视频格式')
+
+def main():
+	path = "C:/Users/Administrator/Desktop/15 淘宝轮播图权重问题.avi"
+	video_type = ["mp4","m4a","mov","avi","wmv","mpeg","rmvb"]
+	audio_type = ["mp3","wav","amr","aac"]
+	if os.path.exists(path):
+		if os.path.isdir(path):
+			file_path = os.path.abspath(path)
+			audio_path = os.path.join(os.path.dirname(file_path),os.path.split(file_path)[1]+"--音频")
+			txt_path = os.path.join(os.path.dirname(file_path),os.path.split(file_path)[1]+"--文本")
+			flag = False
+			if not os.path.exists(audio_path):
+				os.mkdir(audio_path)
+			if not os.path.exists(txt_path):
+				os.mkdir(txt_path)
+			for f in os.listdir(path):
+				video_file = os.path.join(path,f)
+				if f.rsplit('.',1)[-1].lower() in video_type:
+					mp4tomp3(audio_path,video_file)
+					flag = True
+				elif f.rsplit('.',1)[-1].lower() in audio_type:
+					getTxt(txt_path,video_file)
+			if flag:
+				for mf in os.listdir(audio_path):
+					audio_file = os.path.join(audio_path,mf)
+					if mf.rsplit('.',1)[-1].lower() in audio_type:
+						getTxt(txt_path,audio_file)
+		elif os.path.isfile(path):
+			base_path = os.path.dirname(path)
+			if path.rsplit('.',1)[-1].lower() in video_type:
+				mp3_file = os.path.join(base_path,os.path.split(path)[1].rsplit(".",1)[0] + '.mp3')
+				mp4tomp3(base_path,path)
+				getTxt(base_path,mp3_file)
+			elif path.rsplit('.',1)[-1].lower() in audio_type:
+				getTxt(base_path,path)
+	else:
+		print("请输入正确的文件夹或文件地址")
+
+
+if __name__ == "__main__":
+	main()
