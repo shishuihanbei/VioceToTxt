465841835a9a5769f64ac5ba0f1d08f0970a93cb
shishuihanbei <31913640+shishuihanbei@users.noreply.github.com>
2023-03-19 14:43:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from VideoAudiotoTxt import LenovoFileToTxt
 import time
 
 def getTxt(txt_path,video_file):
-	print("正在处理：",os.path.split(video_file)[-1])
+	# print("正在处理：",os.path.split(video_file)[-1])
 	file_name,txt = LenovoFileToTxt(video_file).getTaskTxt()
 	with open(os.path.join(txt_path,file_name.rsplit(".",1)[0].strip()+'.txt'),'w',encoding="utf-8") as f:
 		f.write(txt)
-	print("处理完成：",os.path.split(video_file)[-1])
+	# print("处理完成：",os.path.split(video_file)[-1])
 
 def mp4tomp3(path,file):
 	if not file.endswith('.mp3'):
@@ -20,15 +20,17 @@ def mp4tomp3(path,file):
 		fname,ftype = os.path.splitext(fname)
 		print("正在进行格式转换：",fname)
 		file_mp3 = os.path.join(path,fname) + '.mp3'
-		l = audio.write_audiofile(file_mp3,logger=None)
+		audio.write_audiofile(file_mp3,logger=None)
 		return file_mp3
 	else:
 		print('不是视频格式')
 
 def main():
+	starttime = time.time()
 	path = input("请输入文件或文件夹地址：").replace('"','')
 	video_type = ["mp4","m4a","mov","avi","wmv","mpeg","rmvb"]
 	audio_type = ["mp3","wav","amr","aac"]
+	print("*" * 100)
 	if os.path.exists(path):
 		if os.path.isdir(path):
 			file_path = os.path.abspath(path)
@@ -45,11 +47,13 @@ def main():
 					mp4tomp3(audio_path,video_file)
 					flag = True
 				elif f.rsplit('.',1)[-1].lower() in audio_type:
+					print('-' * 50)
 					getTxt(txt_path,video_file)
 			if flag:
 				for mf in os.listdir(audio_path):
 					audio_file = os.path.join(audio_path,mf)
 					if mf.rsplit('.',1)[-1].lower() in audio_type:
+						print('-' * 50)
 						getTxt(txt_path,audio_file)
 						time.sleep(5)
 		elif os.path.isfile(path):
@@ -62,6 +66,11 @@ def main():
 				getTxt(base_path,path)
 	else:
 		print("请输入正确的文件夹或文件地址")
+	endtime = time.time()
+	releasetime = endtime - starttime
+	m,s = divmod(releasetime,60)
+	h,m = divmod(m,60)
+	print("耗时：{}时{}分{}秒" .format(int(h),int(m),int(s)))
 
 
 if __name__ == "__main__":
