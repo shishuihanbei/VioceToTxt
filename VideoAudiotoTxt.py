ba09e33ce67f7769ab858ddaa7c149f8b47e1bee
shishuihanbei <31913640+shishuihanbei@users.noreply.github.com>
2023-03-19 14:42:43 +0800

    Update VideoAudiotoTxt.py

--- VideoAudiotoTxt.py
+++ VideoAudiotoTxt.py
@@ -3,8 +3,8 @@ import os
 import requests
 import json
 import time
-import random
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+import random
 
 class LenovoFileToTxt():
 	def __init__(self,file):
@@ -42,29 +42,36 @@ class LenovoFileToTxt():
 		cui = "https://cuiauth.lenovo.com.cn:443/auth/v1/simpletoken"
 		resp = requests.post(url = cui,data = json.dumps(data),headers = headers,verify = False)
 		result = resp.json()
+
 		tokencui = result['data']['access_token']
 		return tokencui
 
 	def fileupload(self):
 		"""音视频文件上传"""
-		upload_url = self.url + "/voice/wav2txt"
-		params = {
-			"language":"cn",
-			"videoName":self.videoName,
-			"videoSize":self.videoSize,
-			"period":self.period
-		}
-		files = {'file':(self.videoName,open(self.file,'rb'), 'audio/mpeg')}
-		formatdate = MultipartEncoder(files)
-		headers = self.headers
-		headers['Content-Type'] = formatdate.content_type
 		flag = True
 		while flag:
-			resp = requests.post(url = upload_url,params = params,data= formatdate,headers = headers,verify =False)
-			taskId = resp.json()['res']['taskId']
-			if taskId:
-				flag = False
-		return taskId
+			upload_url = self.url + "/voice/wav2txt"
+			params = {
+				"language":"cn",
+				"videoName":self.videoName,
+				"videoSize":self.videoSize,
+				"period":self.period
+			}
+			files = {'file':(self.videoName,open(self.file,'rb'), 'audio/mpeg')}
+			formatdate = MultipartEncoder(files)
+			headers = self.headers
+			headers['Content-Type'] = formatdate.content_type
+			print("\r正在上传文件：{}" .format(self.videoName))
+			try:
+				resp = requests.post(url = upload_url,params = params,data= formatdate,headers = headers,verify =False)
+				taskId = resp.json()['res']['taskId']
+				if taskId:
+					flag = False
+					return taskId
+			except:
+				print('准备重新上传！')
+				time.sleep(30)
+			continue
 
 	def getTaskStatus(self,taskId):
 		"""获取任务状态"""
@@ -83,19 +90,37 @@ class LenovoFileToTxt():
 		resp = requests.get(url = del_url, headers = self.headers, params = params,verify =False)
 		result = resp.json()
 		status = result['status']
-		# if status == "Y":
-		# 	print(self.videoName + "文件删除成功！")
+		if status == "Y":
+			print("\n远程文件删除成功！")
 
 	def getTaskTxt(self):
 		"""获取完成的文件并删除任务"""
 		taskId = self.fileupload()
 		flag = True
+		n = 1
 		while flag:
+			print("\r正在查询是否转换完成,当前查询：{}次" .format(n),end=" ")
 			result = self.getTaskStatus(taskId)
 			translateTime = result['res']['translateTime']
+			n += 1
 			if translateTime == "已完成":
 				flag = False
 				self.deleteTask(taskId)
 				txt = "\n".join([i['onebest'] for i in json.loads(result["res"]["asrTxt"])])
+				print("\r输出完成...")
 				return self.videoName,txt
-			time.sleep(1)
+			time.sleep(5)
+
+def main():
+	file = "C:/Users/Administrator/Desktop/test/"
+	for i in os.listdir(file):
+		print("正在处理",i)
+		f = os.path.join(file,i)
+		file_name,txt = LenovoFileToTxt(f).getTaskTxt()
+		with open(os.path.join(file,file_name.rsplit(".",1)[0].strip()+'.txt'),'w',encoding="utf-8") as f:
+			f.write(txt)
+		# path = os.path.abspath(f)
+		print(file)
+
+if __name__ == '__main__':
+	main()
